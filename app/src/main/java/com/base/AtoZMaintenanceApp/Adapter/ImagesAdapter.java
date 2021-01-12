e7303ec39015553e3ca669cefadd2048ba114343
tushar <tusharpandey1993@gmail.com>
2021-01-12 18:44:11 +0530

    gallery view

---
+++ app/src/main/java/com/base/AtoZMaintenanceApp/Adapter/ImagesAdapter.java
@@ -0,0 +1,56 @@
+package com.base.AtoZMaintenanceApp.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.base.AtoZMaintenanceApp.R;
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import pl.aprilapps.easyphotopicker.MediaFile;
+
+public class ImagesAdapter extends RecyclerView.Adapter<ImagesAdapter.ViewHolder> {
+
+    private Context context;
+    private List<MediaFile> imagesFiles;
+
+    public ImagesAdapter(Context context, List<MediaFile> imagesFiles) {
+        this.context = context;
+        this.imagesFiles = imagesFiles;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        return new ViewHolder(inflater.inflate(R.layout.view_image, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Glide.with(context)
+                .load(imagesFiles.get(position).getFile())
+                .into(holder.imageView);    }
+
+    @Override
+    public int getItemCount() {
+        return imagesFiles.size();
+    }
+
+    protected static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ImageView imageView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.image_view);
+        }
+
+
+    }
+}
--- app/src/main/java/com/base/AtoZMaintenanceApp/Fragments/ReportFragment.java
+++ app/src/main/java/com/base/AtoZMaintenanceApp/Fragments/ReportFragment.java
@@ -1,10 +1,14 @@
 package com.base.AtoZMaintenanceApp.Fragments;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,13 +16,28 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.base.AtoZMaintenanceApp.Adapter.ImagesAdapter;
 import com.base.AtoZMaintenanceApp.CommonFiles.Utility;
 import com.base.AtoZMaintenanceApp.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import pl.aprilapps.easyphotopicker.ChooserType;
+import pl.aprilapps.easyphotopicker.DefaultCallback;
+import pl.aprilapps.easyphotopicker.EasyImage;
+import pl.aprilapps.easyphotopicker.MediaFile;
+import pl.aprilapps.easyphotopicker.MediaSource;
 
 public class ReportFragment extends BaseFragment implements View.OnClickListener {
 
@@ -34,6 +53,24 @@ public class ReportFragment extends BaseFragment implements View.OnClickListener
 
     FragmentActivity mActivity;
 
+    private static final String PHOTOS_KEY = "easy_image_photos_list";
+    private static final int CHOOSER_PERMISSIONS_REQUEST_CODE = 7459;
+    private static final int CAMERA_REQUEST_CODE = 7500;
+    private static final int CAMERA_VIDEO_REQUEST_CODE = 7501;
+    private static final int GALLERY_REQUEST_CODE = 7502;
+    private static final int DOCUMENTS_REQUEST_CODE = 7503;
+
+    protected RecyclerView recyclerView;
+
+    protected View galleryButton;
+
+    private ImagesAdapter imagesAdapter;
+
+    private ArrayList<MediaFile> photos = new ArrayList<>();
+
+    private EasyImage easyImage;
+
+
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -45,21 +82,71 @@ public class ReportFragment extends BaseFragment implements View.OnClickListener
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        mView = inflater.inflate(R.layout.fragment_register, container, false);
-        editMobileNumber = (EditText) mView.findViewById(R.id.edit_mobile_number);
-        editEmail = (EditText) mView.findViewById(R.id.edit_email);
-        editName = (EditText) mView.findViewById(R.id.edit_name);
-        btnRegister = (Button) mView.findViewById(R.id.btnRegister);
-        layoutAlreadyAccount = (ConstraintLayout) mView.findViewById(R.id.layoutAlreadyAccount);
-        btnRegister.setOnClickListener(this);
-        layoutAlreadyAccount.setOnClickListener(this);
-        txt_privacy_policy = (TextView) mView.findViewById(R.id.txt_privacy_policy);
-        txt_privacy_policy.setOnClickListener(this);
+        mView = inflater.inflate(R.layout.report, container, false);
 
+        recyclerView = mView.findViewById(R.id.recycler_view);
+
+
+        if (savedInstanceState != null) {
+            photos = savedInstanceState.getParcelableArrayList(PHOTOS_KEY);
+        }
+
+        imagesAdapter = new ImagesAdapter(mActivity, photos);
+        recyclerView.setLayoutManager(new LinearLayoutManager(mActivity));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(imagesAdapter);
+
+        easyImage = new EasyImage.Builder(mActivity)
+                .setChooserTitle("Pick media")
+                .setCopyImagesToPublicGalleryFolder(false)
+//                .setChooserType(ChooserType.CAMERA_AND_DOCUMENTS)
+                .setChooserType(ChooserType.CAMERA_AND_GALLERY)
+                .setFolderName("EasyImage sample")
+                .allowMultiple(true)
+                .build();
+
+        checkGalleryAppAvailability();
+
+        mView.findViewById(R.id.chooser_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] necessaryPermissions = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                if (arePermissionsGranted(necessaryPermissions)) {
+                    easyImage.openChooser(mActivity);
+                } else {
+                    requestPermissionsCompat(necessaryPermissions, CHOOSER_PERMISSIONS_REQUEST_CODE);
+                }
+            }
+        });
 
         return mView;
     }
 
+
+    private void checkGalleryAppAvailability() {
+        if (!easyImage.canDeviceHandleGallery()) {
+            //Device has no app that handles gallery intent
+            galleryButton.setVisibility(View.GONE);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == CHOOSER_PERMISSIONS_REQUEST_CODE && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            easyImage.openChooser(mActivity);
+        } else if (requestCode == CAMERA_REQUEST_CODE && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            easyImage.openCameraForImage(mActivity);
+        } else if (requestCode == CAMERA_VIDEO_REQUEST_CODE && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            easyImage.openCameraForVideo(mActivity);
+        } else if (requestCode == GALLERY_REQUEST_CODE && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            easyImage.openGallery(mActivity);
+        } else if (requestCode == DOCUMENTS_REQUEST_CODE && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            easyImage.openDocuments(mActivity);
+        }
+    }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
@@ -263,5 +350,54 @@ public class ReportFragment extends BaseFragment implements View.OnClickListener
     public void onResume() {
         super.onResume();
 //        checkFormFilled();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        easyImage.handleActivityResult(requestCode, resultCode, data, mActivity, new DefaultCallback() {
+            @Override
+            public void onMediaFilesPicked(MediaFile[] imageFiles, MediaSource source) {
+                for (MediaFile imageFile : imageFiles) {
+                    Log.d("EasyImage", "Image file returned: " + imageFile.getFile().toString());
+                }
+                onPhotosReturned(imageFiles);
+            }
+
+            @Override
+            public void onImagePickerError(@NonNull Throwable error, @NonNull MediaSource source) {
+                //Some error handling
+                error.printStackTrace();
+            }
+
+            @Override
+            public void onCanceled(@NonNull MediaSource source) {
+                //Not necessary to remove any files manually anymore
+            }
+        });
+    }
+
+    private void onPhotosReturned(@NonNull MediaFile[] returnedPhotos) {
+        photos.addAll(Arrays.asList(returnedPhotos));
+        imagesAdapter.notifyDataSetChanged();
+        recyclerView.scrollToPosition(photos.size() - 1);
+    }
+
+    private boolean arePermissionsGranted(String[] permissions) {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(mActivity, permission) != PackageManager.PERMISSION_GRANTED)
+                Log.d("KL", "arePermissionsGranted: ");
+         /*   startActivity(new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                    Uri.parse("package:" + BuildConfig.APPLICATION_ID)));
+*/
+            return false;
+
+        }
+        return true;
+    }
+
+    private void requestPermissionsCompat(String[] permissions, int requestCode) {
+        ActivityCompat.requestPermissions(mActivity, permissions, requestCode);
     }
 }
